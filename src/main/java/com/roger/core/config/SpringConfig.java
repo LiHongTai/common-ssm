130fb1d8bc74abe77b847ab835a8638d816aff47
LiHongTai <631396529@qq.com>
2019-02-13 15:01:19 +0800

    添加一些必要的代码，便于理解

--- src/main/java/com/roger/core/config/SpringConfig.java
+++ src/main/java/com/roger/core/config/SpringConfig.java
@@ -3,7 +3,6 @@ package com.roger.core.config;
 import com.roger.core.constant.SystemConstant;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * Spring管理容器
